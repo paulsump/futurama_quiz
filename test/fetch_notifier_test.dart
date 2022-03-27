24f728cfb347e860e3a5ee5e1c408ed5ad21d5ec
paulsump <sumpner@hotmail.com>
2022-03-27 10:38:28 +0100

    Added getCharacters() test. (passing)

--- test/fetch_notifier_test.dart
+++ test/fetch_notifier_test.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter_test/flutter_test.dart';
+import 'package:futurama_quiz/character.dart';
 import 'package:futurama_quiz/fetch_notifier.dart';
 import 'package:http/http.dart' as http;
 import 'package:http/testing.dart';
@@ -10,6 +11,8 @@ import 'package:mockito/annotations.dart';
 // Create new instances of this class in each test.
 @GenerateMocks([http.Client])
 void main() {
+  String fixture(String name) => File('test_data/$name').readAsStringSync();
+
   group('fetcher.getInfo()', () {
     test('returns a List if the http call completes successfully', () async {
       final client = MockClient((_) async => http.Response(
@@ -31,6 +34,31 @@ void main() {
 
       final fetcher = Fetcher(client);
       expect(fetcher.getInfo(), throwsException);
+    });
+  });
+
+  group('fetcher.getCharacters()', () {
+    test('returns a List if the http call completes successfully', () async {
+      final client = MockClient((_) async => http.Response(
+              fixture('characters.json'), 200, headers: {
+            HttpHeaders.contentTypeHeader: 'application/json; charset=utf-8'
+          }));
+
+      final fetcher = Fetcher(client);
+      final characterList = await fetcher.getCharacters();
+
+      expect(
+          characterList
+              .map((character) => Character.fromJson(character))
+              .toList(),
+          isA<List<Character>>());
+    });
+
+    test('throws an exception if the http call completes with an error', () {
+      final client = MockClient((_) async => http.Response('Not Found', 404));
+
+      final fetcher = Fetcher(client);
+      expect(fetcher.getCharacters(), throwsException);
     });
   });
 }
